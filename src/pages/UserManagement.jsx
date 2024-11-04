aa627e9e49bd0aea725a1e7fbdb7bd19f7dacbca
mercy-rotich <cherotichm182@gmail.com>
2024-11-04 13:02:52 -0800

    added a confirmstion modal for the delete button

--- src/pages/UserManagement.jsx
+++ src/pages/UserManagement.jsx
@@ -118,7 +118,7 @@ const UserManagement = () => {
                   <td className="py-[0.5rem] px-[1rem]">
                     <button
                       className="bg-red-500 text-white px-[1rem] py-[0.5rem]"
-                      onClick={() => handleDelete(index)}
+                      onClick={() => openModal(index)}
                     >
                       Delete
                     </button>
@@ -147,20 +147,23 @@ const UserManagement = () => {
         </div>
       </div>
 
-      {isModalOpen &&(
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded shadow-lg w-96">
-             <h2 className="text-lg font-semibold mb-4">Confirm Delete</h2>
-             <p>are you sure you want to delete this user?</p>
-             <div className="mt-6 flex justify-end space-x-4">
-                  <button onClick={closeModal} className="bg-gray-300 text-black px-4 py-2 rounded">
-                    Confirm
-                  </button>
-             </div>
-          </div>
+      {isModalOpen && (
+  <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+    <div className="bg-white p-6 rounded shadow-lg w-96">
+      <h2 className="text-lg font-semibold mb-4">Confirm Delete</h2>
+      <p>Are you sure you want to delete this user?</p>
+      <div className="mt-6 flex justify-end space-x-4">
+        <button onClick={closeModal} className="bg-gray-300 text-black px-4 py-2 rounded">
+          Cancel
+        </button>
+        <button onClick={() => handleDelete(userToDelete)} className="bg-red-500 text-white px-4 py-2 rounded">
+          Confirm
+        </button>
+      </div>
+    </div>
+  </div>
+)}
 
-        </div>
-      )}
     </div>
   );
 };
